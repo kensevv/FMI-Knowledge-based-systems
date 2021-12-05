f6bb0c86da10db57cf9c1fd60bf4a0bf73664e10
Kenan Yusein <kenan.yusein@nevexis.com>
2021-12-06 01:30:39 +0200

    Dialog for File content displaying

---
+++ fmi-plagiarism/frontend/src/components/FileContentDialog.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin" style="min-width: 70vh; width: auto; max-width: 140vh">
+      <q-card-section>
+        <div class="text-h6"> Content of "{{ dataFile.fileName }}"</div>
+      </q-card-section>
+      <q-separator/>
+      <q-card-section style="overflow-wrap: break-word;" class="scroll">
+        {{ dataFile.text }}
+      </q-card-section>
+      <q-separator/>
+      <q-card-actions align="right">
+        <q-btn flat label="Close" color="primary" v-close-popup/>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts" setup>
+import {useDialogPluginComponent} from "quasar";
+
+const {dialogRef, onDialogHide, onDialogOK, onDialogCancel} = useDialogPluginComponent()
+
+const props = defineProps<{
+  dataFile: DataFiles,
+}>();
+
+defineEmits([...useDialogPluginComponent.emits])
+</script>
+
+<style scoped>
+
+</style>
--- fmi-plagiarism/frontend/src/pages/Administration.vue
+++ fmi-plagiarism/frontend/src/pages/Administration.vue
@@ -1,17 +1,69 @@
 <template>
   <q-page padding>
     <div class="row">
+      <div class="col-2"></div>
       <div class="col">
-        <q-table title="Data Files" :rows="data"></q-table>
+        <q-table title="Data Files"
+                 hide-pagination
+                 virtual-scroll
+                 :pagination="{rowsPerPage: 0}"
+                 no-data-label="I didn't find anything for you"
+                 bordered
+                 :rows="data"
+                 :columns="columns">
+          <template v-slot:top-right>
+            <q-input v-model="filter" clearable debounce="300" dense placeholder="Search">
+              <template v-slot:append>
+                <q-icon name="search"/>
+              </template>
+            </q-input>
+          </template>
+          <template v-slot:body-cell-manage="props">
+            <q-td class="text-left">
+              <q-btn color="secondary" icon="info_outline" no-caps flat size="15px" round
+                     @click="()=>showContent(props.row)">
+                <q-tooltip style="font-size: large">
+                  Click to see file content
+                </q-tooltip>
+              </q-btn>
+            </q-td>
+          </template>
+        </q-table>
       </div>
+      <div class="col-2"></div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts" setup>
 import {getAllDataFiles} from "../services/request-service";
+import {ref} from "vue";
+import {useQuasar} from "quasar";
+import FileContentDialog from "../components/FileContentDialog.vue";
+
+const quasar = useQuasar()
 
 const data = await getAllDataFiles()
+
+const filter = ref('')
+
+const showContent = (dataFileRecord: DataFiles) => {
+  quasar.dialog({
+    component: FileContentDialog,
+    componentProps: {
+      'dataFile': {...dataFileRecord},
+    }
+  })
+}
+
+const columns = [
+  {name: "id", label: "UUID", field: "id", align: "left", sortable: true},
+  {name: "fileName", label: "File Name", field: "fileName", align: "left", sortable: true},
+  {name: "uploadDate", label: "Upload Date", field: "uploadDate", align: "left", sortable: true},
+  {name: "verified", label: "Verified", field: "verified", align: "left", sortable: true},
+  {name: "plagiarism", label: "Plagiarism Detected", field: "plagiarismDetected", align: "left", sortable: true},
+  {name: "manage"}
+]
 </script>
 
 <style scoped>
